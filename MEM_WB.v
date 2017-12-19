7fc14d73549d9b4de1acbd905270dfd8696714d2
Ruben Coucke <rubencoucke@MacBook-Air-van-Ruben.local>
2017-12-19 12:49:23 +0100

    tot p 315

--- MEM_WB.v
+++ MEM_WB.v
@@ -1,19 +1,24 @@
-module MEM_WB(read_data_in, read_data_uit, clock, reset, alu_result_in, alu_result_uit);
+module MEM_WB(write_register_in, write_register_out,read_data_in, read_data_uit, clock, reset, alu_result_in, alu_result_uit);
 
 input clock, reset;
 input [63:0] read_data_in, alu_result_in;
+input [4:0] write_register_in;
 
 output [63:0] read_data_uit, alu_result_uit;
+output [4:0] write_register_out;
+reg [4:0] write_register_out;
 reg [63:0] read_data_uit, alu_result_uit;
 
 always @(posedge clock)begin
 	if(reset)begin
 		read_data_uit <= 64'b0;
 		alu_result_uit <= 64'b0;
+		write_register_out <= 5'b0;
 	end
 	else begin
 		read_data_uit <= read_data_in;
 		alu_result_uit <= alu_result_in;
+		write_register_out <= write_register_in;
 	end 
 end
 endmodule 
--- execution.v
+++ execution.v
@@ -1,11 +1,15 @@
-module execution(pc_out,sign_extend_out,read_data_1,read_data_2,add_out, zero, alu_result,write_data, ALUSrc);
+module execution(pc_out,sign_extend_out,read_data_1,read_data_2,add_out, zero, alu_result,write_data, ALUSrc, ALUOp, instruction);
 
 input [63:0] pc_out, read_data_1,read_data_2, sign_extend_out;
 input ALUSrc;
+input [1:0] ALUOp;
+input [31:0] instruction;
 wire [3:0] alu_control_uit;
 
 output [63:0] add_out, alu_result, write_data;
 output zero;
+
+wire [63:0] mux_out;
 
 wire [63:0] shift_left_2_out;
 
@@ -16,5 +20,9 @@ n_mux n_mux(read_data_2, sign_extend_out, mux_out, ALUSrc);
 alu alu(read_data_1,mux_out,alu_result, zero, alu_control_uit);
 
 shift_left_2 shift_left_2(sign_extend_out,shift_left_2_out);
+
+alu_control alu_control(.alu_op(ALUOp),
+						.instruction(instruction[31:21]),
+						.out(alu_control_uit));
 
 endmodule 
